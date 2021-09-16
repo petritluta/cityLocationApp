1372fcd2158062ccf0b22c5792921f512079f915
=Petrit Luta <petrit.luta@gmail.com>
2021-09-16 22:29:09 +0200

    Initial Commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { MapPin, Home, Lock, Users } from "react-feather";
+class Navbar extends React.Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light py-3 app-navbar">
+        <div className="container">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/#">
+                  <MapPin />
+                  Cities
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/#">
+                  <Home />
+                  Hotels
+                </a>
+              </li>
+            </ul>
+
+            <div class="mx-auto d-none d-sm-none d-md-none d-lg-block">
+              <a className="navbar-brand" href="/#">
+                <img
+                  src={`${process.env.PUBLIC_URL}/images/logo-horizontal.png`}
+                  alt="Good dogo"
+                  style={{ width: 150, height: 62 }}
+                />
+              </a>
+            </div>
+
+            <ul className="navbar-nav me-auto ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/#">
+                  <Lock />
+                  Login
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/#">
+                  <Users />
+                  Register
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
---
+++ src/components/Newsletter.js
@@ -0,0 +1,9 @@
+import React, { Component, useState } from "react";
+
+const NewsLetter = () => {
+  const [formData, setformData] = useState(0);
+
+  return null;
+};
+
+export default NewsLetter;
